31df31aeb3f8f62597d2c3e8a683e917096279c9
José Luiz <jose.iscosistemas@outlook.com>
2018-03-21 16:27:17 -0300

    co

--- js/mais.js
+++ js/mais.js
@@ -6,7 +6,7 @@ function Opneva() {
     t = window.innerHeight - height;
     l = window.innerWidth - width - 10;
 
-    window.open('https://bots4app.azurewebsites.net/chat.html?appid=0012ss22sfs23333&avatar=avatarBot.png&botname=Ian - Agente Virtual&placeholder=Ex. Preciso de um sistema, Suporte Técnico&userid=Cliente&s=_sCtJ_yiHjQ.cwA.nyc.OM1xde4Obm9oI1RonXvMblBAhJEgUrLftpLNhSxONWs&css=botconfig ', '', 'height=' + height + ', width=' + width + ', left=' + l + ', top=' + t);
+    window.open('https://bots4app.azurewebsites.net/chat.html?appid=0012ss22sfs23333&avatar=avatarBot.png&botname=Ian - Agente Virtual&placeholder=Ex. Preciso de um sistema, Suporte Técnico&userid=Cliente&username=Você&s=_sCtJ_yiHjQ.cwA.nyc.OM1xde4Obm9oI1RonXvMblBAhJEgUrLftpLNhSxONWs&css=botconfig ', '', 'height=' + height + ', width=' + width + ', left=' + l + ', top=' + t);
 }
 
 
